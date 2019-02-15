4b14dfae096a572192465fcc5ea60bc82cbf3132
Wei-Chun <chu@fitanalytics.com>
2019-02-15 13:03:50 +0100

    refactor

--- report_2_users.R
+++ report_2_users.R
@@ -28,10 +28,14 @@ d <-
 ## Now this dataset has 5 predictor features: lat, lon, month, day, and hour.
 ## The responses usertype, b_year, and gender will be predicted independently from
 ## the 5 features.
-colnames_feat <- c("lat", "lon", "month", "wday", "hour")
+feat_names <- c("lat", "lon", "month", "wday", "hour")
 
-## create function to split training and test set
-split_train_test <- function(d, t = 0.2) {
+## for tensorflow
+feat_cols <- feature_columns(
+  column_numeric("lat", "lon", "month", "wday", "hour"))
+
+## function to split training and test set
+splitter <- function(d, t = 0.2) {
   # d is the input data
   # t is the proportion of test (0 < t < 1)
   if (t <= 0 | t >= 1) {
@@ -43,6 +47,17 @@ split_train_test <- function(d, t = 0.2) {
     train = d[-s, ],
     test = d[s, ]
   )
+}
+
+## function to sample max of N rows from data frame
+sampler <- function(d, m = 10000) {
+  n <- nrow(d)
+  if (m >= n) {
+    warning(sprintf("data size %d smaller than desired size, return original", n))
+    return(d)
+  }
+  s <- sample(x = n, size = m, replace = F)
+  d[s, ]
 }
 
 
@@ -57,46 +72,42 @@ d_usertype <-
     d_subscribers[sample(x = nrow(d_subscribers),
                          size = nrow(d_customers),
                          replace = F), ]
-  )
+  ) %>%
+  mutate(usertype = ifelse(usertype == "Subscriber", 1, 0)) %>%
+  sampler(m = 50000) ## reduce data size for development
 
 ## split training and test sets with 80%-20%
-d_usertype_s <- split_train_test(d_usertype)
+d_usertype_s <- splitter(d_usertype)
 
 ## (1) random forest
 rf_usertype <-
   randomForest(
-    x = d_usertype_s$train[, colnames_feat],
+    x = d_usertype_s$train[, feat_names],
     y = as.factor(d_usertype_s$train[, "usertype"]),
-    xtest = d_usertype_s$test[, colnames_feat],
+    xtest = d_usertype_s$test[, feat_names],
     ytest = as.factor(d_usertype_s$test[, "usertype"])
   )
 print(rf_usertype) ## error rate ~ 31%
 
 
-## (2) tensorflow
+## (2) tensorflow linear classifier
 
-usertype_classifier <-
-  linear_classifier(
-    feature_columns = feature_columns(
-      column_numeric("lat", "lon", "month", "wday", "hour"))
-  )
-
-train_and_evaluate(
-  usertype_classifier,
+usertype_input_fn <- function(d) {
   input_fn(usertype ~ lat + lon + month + wday + hour,
-           data = d_usertype_s$train,
-           batch_size = 32,
-           epochs = 3))
+           data = d,
+           batch_size = 100,
+           epochs = 3)
+}
 
-predictions <-
-  predict(
-    usertype_classifier,
-    input_fn = input_fn(usertype ~ lat + lon + month + wday + hour,
-                        data = d_usertype_s$test,
-                        batch_size = 32,
-                        epochs = 1),
-    predict_keys = "logistic")
+usertype_classifier <- linear_classifier(feature_columns = feat_cols)
 
+train(usertype_classifier,
+      input_fn = usertype_input_fn(d_usertype_s$train))
+
+predictions <- predict(usertype_classifier,
+                       input_fn = usertype_input_fn(d_usertype_s$test))
+evaluation <- evaluate(usertype_classifier,
+                       input_fn = usertype_input_fn(d_usertype_s$test))
 
 ## 2. predict the gender
 
